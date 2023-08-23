6f2de354f6ee76b59fd6ad8413784884fa2c82de
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-23 18:56:55 +0300

    ..

---
+++ 1.Recursion and Backtrackin Lab/recursive_array_sum_1.py
@@ -0,0 +1,9 @@
+def calc_num(nums, idx):
+    if idx == len(nums) - 1:
+        return nums[idx]
+    return nums[idx] + calc_num(nums, idx + 1)
+
+
+nums = [int(x) for x in input().split()]
+
+print(calc_num(nums, 0))
