094e8955e115766c0d320fd3dc825ac9b4f04495
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-09-04 17:22:10 +0300

    ..

--- 1.Recursion and Backtrackin Lab/recursive_factorial_2.py
+++ 1.Recursion and Backtrackin Lab/recursive_factorial_2.py
@@ -1,9 +1,16 @@
+# TODO from datetime import
+
 def calc_fact(n):
     if n == 1:
         return n
 
-    return n * calc_fact(n-1)
+    return n * calc_fact(n - 1)
 
 
 n = int(input())
 print(calc_fact(n))
+
+res = 1
+for i in range(1, n + 1):
+    res *= i
+print(res)
