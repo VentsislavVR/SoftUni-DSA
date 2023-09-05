f58f931ecba939be3e8dc9ddbe34bb958ac77d0d
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-09-05 22:52:53 +0300

    ..
    ..

---
+++ 1.Recursion and Backtrackin Lab/recursive_fibonacci_7.py
@@ -0,0 +1,16 @@
+def calc_fib(num):
+    if num <=1:
+        return 1
+    return calc_fib(num-1) + calc_fib(num-2)
+
+
+def iterative_fib(num):
+    fib0=1
+    fib1=1
+    res = 0
+    for _ in range(num-1):
+        res = fib0 + fib1
+        fib0,fib1 = fib1,res
+    return res
+
+print(iterative_fib(100))
