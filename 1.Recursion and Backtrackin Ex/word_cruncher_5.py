27033231b04a27348492cd05fa5ae94ba2ae20af
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-09-06 12:22:47 +0300

    ..
    ..

--- 1.Recursion and Backtrackin Ex/word_cruncher_5.py
+++ 1.Recursion and Backtrackin Ex/word_cruncher_5.py
@@ -1,3 +1,22 @@
+def find_all_solutions(idx, target, words_by_idx, words_count, used_words):
+    if idx >= len(target):
+        print(' '.join(used_words))
+        return
+    if idx not in words_by_idx:
+        return
+
+    for word in words_by_idx[idx]:
+        if words_count[word] == 0:
+            continue
+        used_words.append(word)
+        words_count[word] -= 1
+
+        find_all_solutions(idx + len(word), target, words_by_idx, words_count, used_words)
+
+        used_words.pop()
+        words_count[word] += 1
+
+
 words = input().split(', ')
 target = input()
 
@@ -7,8 +26,9 @@ words_count = {}
 for word in words:
     if word in words_count:
         words_count[word] += 1
-    else:
-        words_count[word] = 1
+        continue
+
+    words_count[word] = 1
 
     try:
         idx = 0
@@ -21,23 +41,4 @@ for word in words:
     except ValueError:
         pass
 
-
-def find_all_solutions(idx, target,words_by_idx, words_count, used_words):
-    if idx >= len(target):
-        print(' '.join(used_words))
-        return
-    if idx in words_by_idx:
-        return
-
-    for word in words_by_idx[idx]:
-        used_words.append(word)
-        words_count[word] -=1
-
-        find_all_solutions(idx + len(word), words_by_idx, words_count, used_words)
-
-        used_words.pop()
-        words_count[word] += 1
-
-
-
-find_all_solutions(0,target, words_by_idx, words_count, [])
+find_all_solutions(0, target, words_by_idx, words_count, [])
